490b86bc93d6a7541f7f3d4beb8d04eeba52e72f
staskukhar <staskuk2005@gmail.com>
2023-07-08 21:31:56 +0300

    added routes for new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInWithGoogleComponent } from './components/sign-in-with-google/sign-in-with-google.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'login', component: SignInWithGoogleComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
